f75b9d40adcbfdd84dff07a8e544766bc6387d26
Kyle Carter <kylecarter@zipcoders-MBP-9.T-mobile.com>
2019-02-13 23:47:51 -0500

    updated Animal Factory tests

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,6 +1,13 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
@@ -13,19 +20,34 @@ public class AnimalFactoryTest {
     public void testCreateDog()
     {
         //Given
+        String name = "Simba";
+        Date birthDate = new Date();
+        Dog dog = AnimalFactory.createDog(name, birthDate);
 
         //When
+        String actualName = dog.getName();
+        Date actualBirthDaate = dog.getBirthDate();
 
         //Then
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(birthDate, actualBirthDaate);
+
     }
 
     @Test
     public void testCreateCat()
     {
         //Given
+        String name = "Whisky";
+        Date birthDate = new Date();
+        Cat cat = AnimalFactory.createCat(name, birthDate);
 
         //When
+        String actualName = cat.getName();
+        Date actualBirthDaate = cat.getBirthDate();
 
         //Then
+        Assert.assertEquals(name, actualName);
+        Assert.assertEquals(birthDate, actualBirthDaate);
     }
 }
